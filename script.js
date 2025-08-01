59689d677a021f3438d53d4df2289114716f32e1
matheus <pereira.matheus31@escola.pr.gov.br>
2025-08-01 08:37:32 -0300

    script.js

--- script.js
+++ script.js
@@ -6,13 +6,3 @@ const texto = document.getElementById('texto');
 botao.addEventListener('click', function () {
   texto.style.color = 'red', 'blue', 'green', 'orange', 'purple', 'pink', 'teal', 'gold', 'magenta';
 });
-
-// Exibe o código JS na página como exemplo
-const codigoVisivel = `const botao = document.getElementById('botao');
-const texto = document.getElementById('texto');
-
-botao.addEventListener('click', function () {
-  texto.style.color = 'red';
-});`;
-
-document.getElementById('codigo-js').textContent = codigoVisivel;
